c565eb43ebf61907ec28b9135d15aedea565a4f5
naveench909 <naveench909@gmail.com>
2021-11-08 13:24:38 +0530

    adding category in Quiz

--- src/API.ts
+++ src/API.ts
@@ -16,8 +16,12 @@ export enum Difficulty {
     MEDIUM = "medium",
     Hard = "hard"
 }
+
+const categoryArray = [9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27];
+const category = categoryArray[Math.floor(Math.random() * categoryArray.length)];
+
 export const fetchQuizQuestions = async (amount : number, difficulty : Difficulty) => {
-    const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=21&difficulty=${difficulty}&type=multiple`;
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
     // console.log(data.results); 
     return data.results.map((question : Question) => ({
